e693d4b0100d6707569da0660cc7bef753e0242c
wangle <251668577@qq.com>
2019-12-10 09:21:59 +0800

    添加注释

--- simpleaop/src/main/java/com/lee/simpleaop/SimpleAOP.java
+++ simpleaop/src/main/java/com/lee/simpleaop/SimpleAOP.java
@@ -9,6 +9,10 @@ import java.lang.reflect.Proxy;
  */
 public class SimpleAOP {
     public static Object getProxy(Object bean,Advice advice) {
+        // newProxyInstance()方法是Java提供的生成动态代理对象的方法,一共有三个参数:
+        // 1. 类加载器
+        // 2. 需要被代理的接口,数组  (被代理的目标)
+        // 3. 代理对象,主要是其中的invoke()方法,应该是一个InvocationHandler对象
         return Proxy.newProxyInstance(SimpleAOP.class.getClassLoader(),bean.getClass().getInterfaces(),advice);
     }
 }
